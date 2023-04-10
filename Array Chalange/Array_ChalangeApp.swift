af67df1c4637b9be29fe455785dc285830b7857d
mykola <mykola.sosula@gmail.com>
2023-04-10 15:31:38 +0300

    Initial Commit

---
+++ Array Chalange/Array_ChalangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Array_ChalangeApp.swift
+//  Array Chalange
+//
+//  Created by Mykola on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Array_ChalangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
